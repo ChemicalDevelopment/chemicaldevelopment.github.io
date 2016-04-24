48b241768f954ee49609ca5f50534e2030a8178b
CadeBrown <brown.cade@gmail.com>
2016-04-24 17:26:31 -0400

    Mad fractal update

--- math/complex.js
+++ math/complex.js
@@ -47,7 +47,7 @@ function sqr(a) {
 
 // a/b
 function div(a, b) {
-  return mul(a, conj(b)).scale(1 / norm_sqr(b));
+  return scale(mul(a, conj(b)), (1 / norm_sqr(b)));
 }
 
 //e^a
@@ -63,4 +63,117 @@ function log(a) {
 //a^b
 function pow(a, b) {
   return exp(mul(b, log(a)));
+}
+
+
+/*
+
+Functions class. Implements basic functions as well as some lesser known ones
+
+*/
+
+
+/*
+
+Riemann zeta function. Sum of the recipricols to some power x
+
+*/
+function zeta(x) {
+  var sign = -1;
+  var sum = [1, 0];
+  var nth_t = [1, 0];
+  var n_x = scale(x, -1);
+  for (var i = 2; i < 20; ++i) {
+    nth_t = scale(pow([i, 0], n_x), sign);
+    sum = add(sum, nth_t);
+    sign *= -1;
+  }
+  var co = sub(1, pow([2, 0], sub(1, x)));
+  return mul(co, sum);
+}
+
+function gamma(x) {
+  var lgx = log(x);
+  var x_n2 = sqr(div([1, 0], x));
+  var x_k_n1 = div([1, 0], x);
+  var _ex = mul(x, lgx);
+  _ex = sub(_ex, x);
+  _ex = sub(_ex, scale(log(scale(x, 1 / (2 * 3.141592653589793238))), 1 / 2));
+  _ex = add(_ex, scale(x_k_n1, 1 / 12));
+  x_k_n1 = mul(x_k_n1, x_n2);
+  _ex = sub(_ex, scale(x_k_n1, 1 / 360));
+  x_k_n1 = mul(x_k_n1, x_n2);
+  _ex = add(_ex, scale(x_k_n1, 1 / 1260));
+  return exp(_ex);
+}
+
+
+
+/*
+
+sqrt(x) ^ 2 = x
+
+*/
+function sqrt(x) {
+  var g = scale(x, 1 / 2);
+  for (var i = 0; i < 8; ++i) {
+    g = div(add(mul(g, g), x), scale(g, 2));
+  }
+}
+
+/*
+
+Product function.
+
+*/
+function prod_p1(x) {
+  var p = [1, 0];
+  var x_i = x;
+  for (var i = 1; i < 1000; ++i) {
+    if (norm(x_i) >= 10000000000) {
+      break;
+    }
+    p = mul(p, div(x_i, sub(x_i, 1)));
+    x_i = mul(x_i, x);
+  }
+  return p;
+}
+
+
+/*
+
+Dragon geometric.
+Custom function
+
+*/
+function dragon_geometric(x) {
+  var sum = 0;
+  for (var i = 1; i < 1000; ++i) {  
+    sum += pow(-1, i) * pow(i, x - i);
+  }
+  return sum;
+}
+
+
+/*
+
+Returns 1 if prime
+
+*/
+function isprime(z) {
+  if (z == 0 || z == 1) {
+    return 0;
+  }
+  if (z == 2) {
+    return 1;
+  }
+  if (z % 2 == 0) {
+    return 0;
+  }
+  for (var c = 3; c < Math.sqrt(z); c += 2) { 
+    if (z % c == 0) {
+      return 0;
+    }
+  }
+  return 1;
 }
