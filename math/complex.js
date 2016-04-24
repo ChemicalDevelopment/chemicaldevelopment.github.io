d4fa9f6ab31bd3d9fc3fa5040d3486c07760a714
CadeBrown <brown.cade@gmail.com>
2016-04-24 11:20:08 -0400

    Added fractal gen

---
+++ math/complex.js
@@ -0,0 +1,61 @@
+/*
+
+Complex functions
+
+*/
+
+
+//Conjugate
+function conj(a) {
+  return [a[0], - a[1]];
+}
+
+//Norm
+function norm(a) {
+  return Math.sqrt(norm_sqr(a));
+}
+
+//Norm * Norm
+function norm_sqr(a) {
+  return a[0] * a[0] + a[1] * a[1];
+}
+
+//scales a by b
+function scale(a, b) {
+  return [a[0] * b, a[1] * b];
+}
+
+//Adds a and b
+function add(a, b) {
+  return [a[0] + b[0], a[1] + b[1]];
+}
+
+//a - b
+function sub(a, b) {
+  return [a[0] - b[0], a[1] - b[1]];
+}
+
+//a * b
+function mul(a, b) {
+  return [a[0] * b[0] - a[1] * b[1], a[0] * b[1] + a[1] * b[0]];
+}
+
+// a/b
+function div(a, b) {
+  return mul(a, conj(b)).scale(1 / norm_sqr(b));
+}
+
+//e^a
+function exp(a) {
+  return scale([Math.cos(a[1]), Math.sin(a[1])], Math.exp(a[0]));
+}
+
+//ln(a)
+function log(a) {
+  return [Math.log(norm(a)), Math.atan(a[1] / a[0])];
+}
+
+//a^b
+function pow(a, b) {
+  return exp(mul(b, log(a)));
+}
